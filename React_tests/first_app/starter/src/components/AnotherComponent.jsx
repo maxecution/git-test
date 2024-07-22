d366f6e68bed02799317639c899957a1a4e397ee
Max <mzimmer90@gmail.com>
2024-07-22 10:45:05 +0100

    added QuickLabs 2 components

---
+++ React_tests/first_app/starter/src/components/AnotherComponent.jsx
@@ -0,0 +1,10 @@
+const AnotherComponent = () => {
+    return(
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Sed mollis, libero nec hendrerit auctor, libero lacus laoreet justo, id interdum.</p>
+        </div>
+    );
+}
+
+export default AnotherComponent;
--- React_tests/first_app/starter/src/components/Main.jsx
+++ React_tests/first_app/starter/src/components/Main.jsx
@@ -3,14 +3,14 @@ const Main = () => {
         name: 'MZ',
         age: 33,
         job: 'Weapon'
-    }
+    };
 return (
     <div>
         <p>My name is {me.name}</p>
         <p>I am {me.age} years old.</p>
         <p>and I am a {me.job}</p>
     </div>
-)
+);
 }
 
 export default Main;
