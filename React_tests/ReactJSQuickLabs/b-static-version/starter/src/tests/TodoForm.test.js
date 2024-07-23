6de692fc1a7c2aaac26e59e4dc89041a5af0a0c8
Max <mzimmer90@gmail.com>
2024-07-23 13:55:03 +0100

    added TodoForm test

---
+++ React_tests/ReactJSQuickLabs/b-static-version/starter/src/tests/TodoForm.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import TodoForm from '../Components/TodoForm';
+
+jest.mock('../Components/utils/DateCreated', () => {
+    return function MockDateCreated() {
+        return <span testid='dateCreated'>Date Created Component</span>
+    }
+});
+
+describe('TodoForm test suite', () => {
+    describe('DateCreated function and render tests', () => {
+        test('it should render a DateCreated component', () => {
+            const testRenderer = create(<TodoForm />);
+            const testInstance = testRenderer.root;
+            const dateCreated = testInstance.find(
+                el => el.type === 'span' && el.props.testid === 'dateCreated'
+            );
+            expect(dateCreated).toBeTruthy();
+            expect(dateCreated.children).toContain('Date Created Component');
+        });
+    });
+})
