5ea93d9a433ff7263cd76b86d61241e96523429a
Max <mzimmer90@gmail.com>
2024-07-24 11:56:03 +0100

    added tests for onChange event on todoForm

--- React_tests/ReactJSQuickLabs/b-static-version/starter/src/tests/TodoForm.test.js
+++ React_tests/ReactJSQuickLabs/b-static-version/starter/src/tests/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { create } from 'react-test-renderer';
+import { act, create } from 'react-test-renderer';
 import TodoForm from '../Components/TodoForm';
 
 jest.mock('../Components/utils/DateCreated', () => {
@@ -19,5 +19,27 @@ describe('TodoForm test suite', () => {
             expect(dateCreated).toBeTruthy();
             expect(dateCreated.children).toContain('Date Created Component');
         });
+    });
+    describe('onChange event tests', () => {
+        test('it should render the new value in the input when todoDescription onChange is activated', () => {
+            const testValue = "Test";
+            const { root: testInstance } = create(<TodoForm />);
+            const descInput = testInstance.findByProps({ name: "todoDescription" })
+
+            expect(descInput.props.value).toBe('');
+            act(() => { descInput.props.onChange({ target: { value: testValue } }) });
+            expect(descInput.props.value).toBe(testValue);
+        })
+    });
+    describe('onChange event tests', () => {
+        test('it should render the new value in the checkbox when todoCompleted onChange is activated', () => {
+            const testValue = true;
+            const { root: testInstance } = create(<TodoForm />);
+            const descInput = testInstance.findByProps({ name: "todoCompleted" })
+
+            expect(descInput.props.checked).toBe(false);
+            act(() => { descInput.props.onChange({ target: { checked: testValue } }) });
+            expect(descInput.props.checked).toBe(testValue);
+        })
     });
 })
