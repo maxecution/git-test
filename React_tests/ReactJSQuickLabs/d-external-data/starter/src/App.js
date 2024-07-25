4d8c0ba4791f2290152819ff071cb7e2fc385d82
Max <mzimmer90@gmail.com>
2024-07-25 09:20:58 +0100

    implemented logic for useEffect and get() method

--- React_tests/ReactJSQuickLabs/d-external-data/starter/src/App.js
+++ React_tests/ReactJSQuickLabs/d-external-data/starter/src/App.js
@@ -1,36 +1,61 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'popper.js';
 import 'jquery';
 import './Components/css/qa.css';
-import sampleTodos from './sampleTodos.json';
+//import sampleTodos from './sampleTodos.json'; (No longer seeded after importing axios and using useEffect)
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import AllTodos from './Components/AllTodos';
 import AddEditTodo from './Components/AddEditTodo';
+import Modal from './Components/utils/Modal'
+
+const TODOSURL = 'http://localhost:4000/todos';
 
 function App() {
 
   const [todos, setTodos] = useState({});
+  const [getError, setGetError] = useState('');
+
   useEffect(() => {
-    setTodos({ todos: sampleTodos });
+    const getData = async () => {
+      setTodos(await getTodos());
+    }
+    setTimeout(() => {
+      getData();
+    }, 5000)
   }, []);
+
+  const getTodos = async () => {
+    try {
+      const res = await axios.get(TODOSURL);
+      return res.data.length ? { todos: res.data } : { error: 'There are no todos stored' }
+    } catch (e) {
+      setGetError(`Data not available from server: ${e.message}`);
+      return { error: `Data not available from server: ${e.message}` };
+    }
+  }
+
   const submitTodo = todo => {
     const updatedTodos = [...todos, todo];
     setTodos(updatedTodos);
   }
 
   return (
-    <div className="container">
-      <Header />
+    <>
+      {getError && <Modal handleClose={() => setGetError('')} message={getError} />}
       <div className="container">
-        <AllTodos data={todos} />
-        <AddEditTodo submitTodo={submitTodo} />
+        <Header />
+        <div className="container">
+          <AllTodos data={todos} />
+          <AddEditTodo submitTodo={submitTodo} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
--- React_tests/ReactJSQuickLabs/d-external-data/starter/src/Components/AllTodos.jsx
+++ React_tests/ReactJSQuickLabs/d-external-data/starter/src/Components/AllTodos.jsx
@@ -4,8 +4,84 @@ import './css/AllTodos.css';
 
 import Todo from './Todo';
 import TodoModel from './utils/Todo.model';
+import { useEffect, useState } from 'react';
 
 const AllTodos = ({ data }) => {
+    const [dataStatus, setDataStatus] = useState({ name: 'loading', message: 'Data is loading...' });
+
+    useEffect(() => {
+        if (data?.error) {
+            setDataStatus({ name: 'error' }, { message: data.error });
+        } else if (data?.todos) {
+            const ds = data.todos.length > 0 ? { name: data, message: null } : { name: 'nodata', message: 'There were no todos previously saved' };
+        } else {
+            setDataStatus({ name: 'loading', message: 'Data is loading...' });
+        }
+    }, [data]);
+
+    const populateTable = () => {
+        if (data?.todos?.length > 0) {
+            return data.todos.map(currentTodo => {
+                const {
+                    todoDescription,
+                    todoDateCreated,
+                    todoCompleted,
+                    _id } = currentTodo;
+                const todo = new TodoModel(
+                    todoDescription,
+                    todoDateCreated,//?.toISOString(),
+                    todoCompleted,
+                    _id)
+                return <Todo todo={todo} key={todo._id} />
+            });
+        };
+        return (
+            <tr>
+                <td id={dataStatus.name} colSpan="3">{dataStatus.message}</td>
+            </tr>
+        );
+    };
+
+    return (
+        <div className="row">
+            <h3>Todos List</h3>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Date Created</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>{populateTable()}</tbody>
+            </table>
+        </div>
+    );
+};
+
+AllTodos.propTypes = {
+    data: PropTypes.oneOfType([
+        PropTypes.exact({
+            todos: PropTypes.arrayOf(
+                PropTypes.exact({
+                    _id: PropTypes.string,
+                    todoDescription: PropTypes.string,
+                    todoDateCreated: PropTypes.string,
+                    todoCompleted: PropTypes.bool
+                })
+            )
+        }),
+        PropTypes.exact({
+            error: PropTypes.string
+        }),
+        PropTypes.exact({})
+    ])
+};
+
+export default AllTodos;
+
+//Original Code, replaced when useEffect was introduced
+/* const AllTodos = ({ data }) => {
     const todos = data.todos?.map(currentTodo => {
         const todo = new TodoModel(currentTodo.todoDescription, currentTodo.todoDateCreated, currentTodo.todoCompleted, currentTodo._id);
         return <Todo todo={todo} key={todo._id} />
@@ -37,8 +113,5 @@ AllTodos.propTypes = {
                 todoCompleted: PropTypes.bool
             })
         )
-    })
-};
-
-export default AllTodos;
+    }) */
 
